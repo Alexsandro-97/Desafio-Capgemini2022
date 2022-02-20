1b79abf48227ee1faa0b69e5b86146e38b7f1c7b
Alexsandro-97 <alexsandroeletrotecnica@gmail.com>
2022-02-20 00:21:19 -0300

    senha forte

---
+++ desafio_2/main.dart
@@ -0,0 +1,56 @@
+void main() {
+  test();
+}
+
+void checkPassword(String password) {
+  bool isUppercase = password.contains(RegExp(r'[A-Z]'));
+  bool isLowercase = password.contains(RegExp(r'[a-z]'));
+  bool isNumber = password.contains(RegExp(r'[0-9]'));
+  bool isChar = password.contains(RegExp(r'[!@#$%^&*()-+]'));
+  bool isLength = password.length > 5;
+
+  if (!isUppercase) {
+    print('''
+São necessários ${6 - password.length < 0 ? 0 : 6 - password.length} caracteres adicionais para tornar sua senha segura!
+Sua senha é: $password e necessita adicionar uma letra maíscula!\n
+''');
+    return;
+  }
+  if (!isLowercase) {
+    print('''
+São necessários ${6 - password.length < 0 ? 0 : 6 - password.length} caracteres adicionais para tornar sua senha segura!
+Sua senha é: $password e necessita adicionar uma letra minuscula!\n
+''');
+    return;
+  }
+  if (!isNumber) {
+    print('''
+São necessários ${6 - password.length < 0 ? 0 : 6 - password.length} caracteres adicionais para tornar sua senha segura!
+Sua senha é: $password e necessita adicionar um número!\n
+''');
+    return;
+  }
+  if (!isChar) {
+    print('''
+São necessários ${6 - password.length < 0 ? 0 : 6 - password.length} caracteres adicionais para tornar sua senha segura!
+Sua senha é: $password e necessita adicionar um caractere especial!\n
+''');
+    return;
+  }
+  if (!isLength) {
+    print('''
+São necessários ${6 - password.length < 0 ? 0 : 6 - password.length} caracteres adicionais para tornar sua senha segura!
+Sua senha é: $password e necessita adicionar mais ${6 - password.length < 0 ? 0 : 6 - password.length} caracteres!\n
+''');
+    return;
+  }
+  if (isUppercase && isLowercase && isNumber && isChar && isLength) {
+    print('Senha Ok!\n');
+    return;
+  }
+}
+
+void test() {
+  final passwordTest = ['Ya3', 'YYSA3AS', 'He11oW@rd'];
+  passwordTest.forEach(checkPassword);
+}
